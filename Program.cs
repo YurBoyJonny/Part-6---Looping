e34f14e4bfd1fe38d42320161da792ecc289bfd7
cheujona853 <cheujona853@2260I-L42-74>
2022-04-04 11:04:42 -0400

    stuff

--- Program.cs
+++ Program.cs
@@ -10,8 +10,29 @@ namespace Part_6___Looping
     {
         static void Main(string[] args)
         {
-
-            Prompter();
+            int menuInput;
+            do
+            {
+                Console.WriteLine("Enter 1 for PROMPTER, 2 for PERCENT PASSING, 3 for ODD SUM, or 4 for RANDOM NUMBERS");
+                while (!Int32.TryParse(Console.ReadLine(), out menuInput)) ;
+                if (menuInput == 1)
+                {
+                    Prompter();
+                }
+                if (menuInput == 2)
+                {
+                    PercentPassing();
+                }
+                if (menuInput == 3)
+                {
+                    OddSum();
+                }
+                if (menuInput == 4)
+                {
+                    RandomNumbers();
+                }
+            } while (menuInput < 1 || menuInput > 4);
+            Console.ReadLine();
         }
         public static void Prompter()
         {
@@ -32,18 +53,62 @@ namespace Part_6___Looping
                 Console.WriteLine("Enter a number in between " + minValue + " and " + maxValue);
                 while (!int.TryParse(Console.ReadLine(), out numberBetween));
             } while (numberBetween <= minValue || numberBetween >= maxValue);
+            Console.WriteLine($"You said {numberBetween}!");
         }
         public static void PercentPassing()
-        { 
-        
+        {
+            Console.WriteLine("---PERCENT PASSING---");
+            Console.WriteLine("Enter how many scores you want to enter");
+            int scoreAmount;
+            while(!Int32.TryParse(Console.ReadLine(), out scoreAmount));
+            int scorePassed = 0;
+            for (int i = 1; i <= scoreAmount; i++)
+            {
+                Console.WriteLine("Enter your score #" + i);
+                int theScore;
+                while (!Int32.TryParse(Console.ReadLine(), out theScore)) ;
+                if (theScore > 70)
+                {
+                    scorePassed = scorePassed + 1;
+                }
+            }
+            int scoreAmountPercent = 100 / scoreAmount;
+            int scorePassedPercent = scorePassed * scoreAmountPercent;
+            Console.WriteLine(scorePassedPercent + "% of scores passed 70");
         }
         public static void OddSum()
-        { 
+        {
+            Console.WriteLine("---ODD SUM---");
+            Console.WriteLine("Enter a number");
+            int numberEntered;
+            while (!Int32.TryParse(Console.ReadLine(), out numberEntered)) ;
 
+            for (int oddNumber = 1; oddNumber < (numberEntered + 1); oddNumber ++)
+            {
+                if (oddNumber % 2 != 0)
+                {
+                    Console.WriteLine(oddNumber.ToString());
+                    Thread.Sleep(700);
+                }
+            }
         }
         public static void RandomNumbers()
-        { 
-
+        {
+            Console.WriteLine("---RANDOM NUMBERS---");
+            int minValue;
+            int maxValue;
+            Console.WriteLine("Enter a minimum value");
+            while (!Int32.TryParse(Console.ReadLine(), out minValue));
+            do{
+                Console.WriteLine("Enter a maximum value");
+                while (!Int32.TryParse(Console.ReadLine(), out maxValue));
+            } while (maxValue < minValue);
+            Random generator = new Random();
+            for (int i = 1; i <= 25; i++)
+            {
+                int numberBetween = generator.Next(minValue, maxValue + 1);
+                Console.WriteLine(numberBetween);
+            }
         }
     }
 }
